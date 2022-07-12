d0f853fa1033c3fb23621f52b546931d1d0d94d2
omaxx <maxx.orlov@gmail.com>
2022-07-12 11:04:02 -0400

    change interface data model

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = requirements_file.read_text().splitlines()
 
 setup(
     name='eznet',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     url='https://github.com/omaxx/eznet',
     license='',
