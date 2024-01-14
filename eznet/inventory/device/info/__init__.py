1da4e463257f509b94d93501f4f50782284dbb46
Maxim Orlov <maxx.orlov@gmail.com>
2024-01-13 19:57:46 -0500

    add chassis/system/interfaces info

--- eznet/inventory/device/info/__init__.py
+++ eznet/inventory/device/info/__init__.py
@@ -5,8 +5,10 @@ from dataclasses import dataclass, field
 import eznet
 
 from .system import System
+from .chassis import Chassis
 
 
 class Device:
     def __init__(self, device: eznet.Device) -> None:
         self.system = System(device)
+        self.chassis = Chassis(device)
