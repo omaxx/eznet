a40a9791dec0d190979e2262e6f599ed94c53b5f
Maxim Orlov <maxx.orlov@gmail.com>
2026-03-18 22:18:53 -0700

    Add more juniper run methods

--- src/eznet/jnpr/run.py
+++ src/eznet/jnpr/run.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
-from typing import Literal
+from typing import Literal, Any
+from xml.etree import ElementTree
+import json
 
 from .device import Device, device_method
 
@@ -82,7 +84,7 @@ async def pfe_cmd(
     fpc: int = 0,
     timeout: int = DEFAULT_CMD_TIMEOUT,
 ) -> str:
-    result = await device.ssh.execute(
+    result = await device.ssh.run(
         f'request pfe execute target fpc{fpc} command "{cmd}" timeout 0',
         timeout=timeout,
     )
@@ -119,3 +121,74 @@ async def re_cmd(
     )
     _check_output_for_errors(device, cmd, result.stdout)
     return result.stdout
+
+
+@device_method(prefix="run")
+async def host_cmd(
+    device: Device,
+    cmd: str,
+    timeout: int = DEFAULT_CMD_TIMEOUT,
+) -> str | None:
+    result = await device.ssh.run(f'request app-engine host-cmd "{cmd}"', timeout=timeout)
+    _check_output_for_errors(device, cmd, result.stdout)
+    return result.stdout
+
+
+@device_method(prefix="run")
+async def xml_cmd(
+    device: Device,
+    cmd: str,
+    timeout: int = DEFAULT_CMD_TIMEOUT,
+) -> ElementTree.Element:
+    result = await device.ssh.run(f"{cmd} | display xml", timeout=timeout)
+
+    # First check for junos error in stdout
+    _check_output_for_errors(device, cmd, result.stdout)
+
+    output = result.stdout.replace(" xmlns=", " xmlnamespace=").replace("junos:", "")
+    try:
+        xml = ElementTree.fromstring(output)
+    # except etree.XMLSyntaxError:
+    except ElementTree.ParseError:
+        device.logger.warning(f"{device}: run_xml_cmd: xml parse error")
+        raise CommandError("xml parse error")
+
+    return xml
+
+# async def run_lxml_cmd(
+#     device: Device,
+#     cmd: str,
+#     timeout: int = DEFAULT_CMD_TIMEOUT,
+# ) -> Element:
+#     output, _ = await self.ssh.execute(f"{cmd} | display xml", timeout=timeout)
+#
+#     # First check for junos error in stdout
+#     self.check_output_for_errors(cmd, output)
+#
+#     output = output.replace(" xmlns=", " xmlnamespace=").replace("junos:", "")
+#     try:
+#         xml = etree.fromstring(output)
+#     except etree.XMLSyntaxError:
+#         logger.warning(f"{self}: run_xml_cmd: xml parse error")
+#         raise CommandError("xml parse error")
+#
+#     return xml
+#
+
+@device_method(prefix="run")
+async def json_cmd(
+    device: Device,
+    cmd: str,
+    timeout: int = DEFAULT_CMD_TIMEOUT,
+) -> dict[Any, Any]:
+    result = await device.ssh.run(f"{cmd} | display json", timeout=timeout)
+
+    # First check for junos error in stdout
+    _check_output_for_errors(device, cmd, result.stdout)
+
+    json_output = json.loads(result.stdout)
+    if not isinstance(json_output, dict):
+        device.logger.warning(f"{device}: run_json_cmd: json parse error")
+        raise CommandError("json parse error")
+
+    return json_output
